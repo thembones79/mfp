397510af72c6e55ef0a225f598a6bd86d413a9f8
thembones79 <michal.szumnarski@gmail.com>
2022-04-26 08:26:50 +0200

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+const App = () => <MarketingApp />;
+
+export default App;
---
+++ packages/marketing/src/index.js
@@ -0,0 +1,2 @@
+console.log("marketing");
+import("./bootstrap");
