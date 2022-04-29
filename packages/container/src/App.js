e1559d23c1605da6a31e5ad8375b4ba45ee15275
thembones79 <michal.szumnarski@gmail.com>
2022-04-29 11:37:49 +0200

    ubdated text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
-const App = () => <MarketingApp />;
+const App = () => (
+  <div>
+    <h1>Hi there!</h1>
+    <hr />
+    <MarketingApp />
+  </div>
+);
 
 export default App;
